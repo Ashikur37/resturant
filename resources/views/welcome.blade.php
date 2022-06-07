29999487874e3fe3ecc8996aa8bf21dbb6c465d3
ashikur37 <piash3700@gmail.com>
2022-06-07 22:15:09 +0600

    flash_updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -269,7 +269,7 @@ Mei Mei's unconventional dumpling combinations showcase our love for great New E
 
             <div class="col-xs-12 col-sm-6 dis-table-cell">
                 <div class="section-content">
-                    <h2 class="section-content-title">Our Beer</h2>
+                    <h2 class="section-content-title">Enjoy the meal</h2>
                     <div class="section-description">
                         <p class="section-content-para">
                             Astronomy compels the soul to look upward, and leads us from this world to another.  Curious that we spend more time congratulating people who have succeeded than encouraging people who have not. As we got further and further away, it [the Earth] diminished in size.
